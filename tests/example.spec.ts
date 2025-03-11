6baf3ed7e87a28cecf2cae19c1d5b88b6af59925
nuttawut00 <nuttawutinpeng@gmail.com>
2025-03-11 13:04:03 +0700

    comment test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -18,12 +18,12 @@ test('get started link', async ({ page }) => {
 });
 
 
-test('test failed', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('test failed', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'aaa' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'aaa' })).toBeVisible();
+// });
