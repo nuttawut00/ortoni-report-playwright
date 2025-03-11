225905539db3a10542ca3ca4ac4d2caf02ab311a
nuttawut00 <nuttawutinpeng@gmail.com>
2025-03-11 11:08:13 +0700

    init ortoni report

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,14 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+test('test failed', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'aaa' })).toBeVisible();
+});
